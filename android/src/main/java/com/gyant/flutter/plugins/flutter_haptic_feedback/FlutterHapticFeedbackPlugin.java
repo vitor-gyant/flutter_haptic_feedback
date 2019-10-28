ce96d431a05fd996c93f271094556f055ecff3f4
Vitor Magalhães <vitor@gyant.com>
2019-10-28 12:21:40 +0000

    WIP.

--- android/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback/FlutterHapticFeedbackPlugin.java
+++ android/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback/FlutterHapticFeedbackPlugin.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 
+// V2 embedding imports
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+
+// V1 embedding imports
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
@@ -13,22 +20,44 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /**
  * FlutterHapticFeedbackPlugin
  */
-public class FlutterHapticFeedbackPlugin implements MethodCallHandler {
+public class FlutterHapticFeedbackPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
 
-  static Activity activity;
-
-  private FlutterHapticFeedbackPlugin(Registrar registrar){
-
-  }
+  private Activity activity;
+  private MethodChannel methodChannel;
 
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_haptic_feedback");
-    activity = registrar.activity();
-    channel.setMethodCallHandler(new FlutterHapticFeedbackPlugin(registrar));
+    FlutterHapticFeedbackPlugin plugin = new FlutterHapticFeedbackPlugin();
+    plugin.tearUpChannel(registrar.messenger());
+    plugin.activity = registrar.activity();
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+      this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+      this.activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+      this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+      this.activity = null;
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
+
+      // Ignore any invocation if not attached to an activity.
+      if (activity == null) {
+          return;
+      }
 
     View rootView = activity.getWindow().getDecorView().getRootView();
 
@@ -83,4 +112,25 @@ public class FlutterHapticFeedbackPlugin implements MethodCallHandler {
       result.notImplemented();
     }
   }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
+    tearUpChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor());
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
+    tearDownChannel();
+  }
+
+  private void tearUpChannel(BinaryMessenger messenger) {
+    methodChannel = new MethodChannel(messenger, "flutter_haptic_feedback");
+    methodChannel.setMethodCallHandler(this);
+  }
+
+  private void tearDownChannel() {
+    methodChannel.setMethodCallHandler(null);
+    methodChannel = null;
+  }
+
 }
