6bf21f4b2314ee9bb0d65c1f96fdb15fafca5167
manuel <manuel@gyant.com>
2020-01-23 16:29:09 +0000

    Changes to remove v1 embedding and make it v2 embedding complaint. Upgraded pubspec and gradle

--- android/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback/FlutterHapticFeedbackPlugin.java
+++ android/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback/FlutterHapticFeedbackPlugin.java
@@ -1,116 +1,107 @@
 package com.gyant.flutter.plugins.flutter_haptic_feedback;
 
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 
-// V2 embedding imports
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-
-// V1 embedding imports
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.MethodChannel;
+
 
 /**
  * FlutterHapticFeedbackPlugin
  */
-public class FlutterHapticFeedbackPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
-
+public class FlutterHapticFeedbackPlugin implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware {
   private Activity activity;
   private MethodChannel methodChannel;
 
-  public static void registerWith(Registrar registrar) {
-    FlutterHapticFeedbackPlugin plugin = new FlutterHapticFeedbackPlugin();
-    plugin.tearUpChannel(registrar.messenger());
-    plugin.activity = registrar.activity();
-  }
-
-  @Override
-  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
-      this.activity = activityPluginBinding.getActivity();
-  }
-
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-      this.activity = null;
-  }
-
-  @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
-      this.activity = activityPluginBinding.getActivity();
-  }
-
-  @Override
-  public void onDetachedFromActivity() {
-      this.activity = null;
-  }
-
-  @SuppressLint("NewApi")
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-
-    // Ignore any invocation if not attached to an activity.
-    if (activity == null) {
-        return;
-    }
-
-    View rootView = activity.getWindow().getDecorView().getRootView();
-
-    switch (call.method) {
-      case "vibrate":
-        rootView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-        result.success(null);
-        break;
-
-      case "canVibrate":
-        result.success(true);
-        break;
-
-      case "selection":
-      case "light":
-        rootView.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP);
-        result.success(null);
-        break;
-
-      case "success":
-      case "medium":
-        rootView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-        result.success(null);
-
-      case "impact":
-      case "warning":
-      case "error":
-        //The HapticFeedbackConstants is the same of impact but is handled a different way on dart
-        rootView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-        result.success(null);
-        break;
-      case "heavy":
-
-        rootView.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
-        result.success(null);
-        break;
-
-      default:
-        result.notImplemented();
-        break;
-    }
-  }
-
   @Override
   public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
-    tearUpChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor());
+    tearUpChannel(flutterPluginBinding.getBinaryMessenger());
   }
 
   @Override
   public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
     tearDownChannel();
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+    this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    this.activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    this.activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    this.activity = null;
+  }
+
+  @SuppressLint("NewApi")
+  @Override
+  public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+
+    // Ignore any invocation if not attached to an activity.
+    if (activity == null) {
+      return;
+    }
+
+    View rootView = activity.getWindow().getDecorView().getRootView();
+
+    switch (call.method) {
+    case "vibrate":
+      rootView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+      result.success(null);
+      break;
+
+    case "canVibrate":
+      result.success(true);
+      break;
+
+    case "selection":
+    case "light":
+      rootView.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP);
+      result.success(null);
+      break;
+
+    case "success":
+    case "medium":
+      rootView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+      result.success(null);
+    break;
+
+    case "impact":
+    case "warning":
+    case "error":
+      // The HapticFeedbackConstants is the same of impact but is handled a different
+      // way on dart
+      rootView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+      result.success(null);
+      break;
+    case "heavy":
+
+      rootView.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+      result.success(null);
+      break;
+
+    default:
+      result.notImplemented();
+      break;
+    }
   }
 
   private void tearUpChannel(BinaryMessenger messenger) {
--- example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
+++ example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
@@ -1,14 +1,5 @@
 package com.gyant.flutter.plugins.flutter_haptic_feedback_example;
 
-import com.gyant.flutter.plugins.flutter_haptic_feedback;
-
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
 
-public class MainActivity extends FlutterActivity {
-    // TODO(vitor-gyant): Remove this once v2 of GeneratedPluginRegistrant rolls to stable. https://github.com/flutter/flutter/issues/42694
-    @Override
-    public void configureFlutterEngine(FlutterEngine flutterEngine) {
-        flutterEngine.getPlugins().add(new FlutterHapticFeedbackPlugin());
-    }
-}
+public class MainActivity extends FlutterActivity { }
